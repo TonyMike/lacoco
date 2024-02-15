03a884e9ae081ac1e0c3b2a22276925019d23b5c
Tony Michael <tee.jhay1@gmail.com>
2024-02-16 00:30:12 +0100

    created form action

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,7 +1,15 @@
+import { addPost } from "@/lib/actions";
+
 const AdminPage = () => {
   return (
     <div>
-    admin page
+      <form action={addPost} className="flex flex-col gap-4 text-black">
+        <input type="text" name="title" id="" placeholder="title" />
+        <input type="text" name="desc" id="" placeholder="desc" />
+        <input type="text" name="slug" id="" placeholder="slug" />
+        <input type="text" name="userId" id="" placeholder="userId" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/lib/actions.js
@@ -0,0 +1,35 @@
+import { Post } from "./models";
+import { connectToDb } from "./utils";
+
+export const addPost = async (formData) => {
+  "use server";
+
+
+  const { title, desc, slug, userId } = Object.fromEntries(formData)
+  // console.log(title, desc, slug, userId)
+
+  connectToDb()
+  try {
+    const exist = await Post.findOne({ slug: slug })
+    if (exist) {
+      throw new Error('post already exist')
+    } else {
+
+      const post = await Post.create({
+        title: title,
+        desc: desc,
+        slug: slug,
+        userId: userId
+      })
+      console.log('post created')
+      post.save()
+    }
+
+
+  } catch (err) {
+    console.log(err)
+    return { error: 'failed to add post' }
+  }
+
+
+}
